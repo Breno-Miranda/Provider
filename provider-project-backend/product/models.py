f16b8063a46e310319d723438fcf93949a8cb656
Vitoriano <vitorianoernandes2012@gmail.com>
2020-01-10 22:18:17 -0300

    adicionando novos campos no model product

--- provider-project-backend/product/models.py
+++ provider-project-backend/product/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from company.models import Company
 from taxation.models import Tax
-# Create your models here.
+from users.models import Bind, Type
 
 class Status(models.Model):
     company = models.ForeignKey(Company, related_name="status_product",  on_delete=None)
@@ -43,22 +43,35 @@ class Category(models.Model):
     def __str__(self):
         return self.type
   
-class Product(models.Model): 
+class Product(models.Model):
+    reference = models.BigIntegerField(null=True, blank=True)
     company = models.ForeignKey(Company, on_delete=None , blank=True)
     status = models.ForeignKey(Status, on_delete=None , blank=True)
     category = models.ForeignKey(Category, on_delete=None , blank=True)
     description = models.CharField(max_length=250 , null=True, blank=True)
-    ean = models.BigIntegerField(default=False , null=True, blank=True)
-    ncm = models.BigIntegerField(default=False , null=True, blank=True)
+    ean = models.BigIntegerField(default=False, null=True, blank=True)
+    ncm = models.FloatField(default=False, null=True, blank=True)
     unidade = models.CharField(max_length=150 , null=True, blank=True)
     cest = models.BigIntegerField(default=False, null=True, blank=True) 
     amount = models.IntegerField(null=True, blank=True)
     weight = models.IntegerField(null=True, blank=True)
     origin = models.IntegerField(null=True, blank=True)
     discount = models.FloatField(max_length=200 , null=True, blank=True)
-    resale_price = models.DecimalField(max_digits=10, decimal_places=2 , null=True, blank=True)
+    resale_price = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
     sale_price = models.DecimalField(max_digits=10, decimal_places=2) 
-    taxation = models.ForeignKey(Tax, on_delete=None , blank=True) 
+    taxation = models.ForeignKey(Tax, on_delete=None , blank=True)
+    page = models.BigIntegerField(null=True, blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+    value_provider = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
+    type_value_provider = models.CharField(null=True, blank=True)
+    stock = models.BigIntegerField(default=0)
+    size = models.CharField(null=True, blank=True)
+    create_date = models.DateTimeField(auto_now_add=True)
+    due_date = models.DateTimeField(null=True, blank=True)
+    last_date = models.DateTimeField(null=True, blank=True)
+    user = models.ForeignKey(Bind, on_delete=models.CASCADE, null=True, blank=True)
+    user_type = models.ForeignKey(Type, on_delete=models.CASCADE, null=True, blank=True)
+
     
     class Meta:
         managed = True
