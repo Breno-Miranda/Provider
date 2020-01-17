01c5ca391f77e00f7f63d25fafa31f84e359f06f
Breno Miranda <brenomirandaster@gmail.com>
2020-01-17 01:11:07 -0300

    user_bind remove serializer

--- provider-project-backend/users/serializers.py
+++ provider-project-backend/users/serializers.py
@@ -126,7 +126,7 @@ class UsersCreateBindSerializer(serializers.ModelSerializer):
 class UserCreateProfileSerializers(ModelSerializer):
     class Meta:
         model = Profile
-        fields = ("full_name", "matriculation", "company", "user_bind", "bind",
+        fields = ("full_name", "matriculation", "company", "bind",
                   "cpf", "genre", "address")
 
 
