22278a0c3cef38db5fb1a09e6f5d1932608da49e
Breno Miranda <brenomirandaster@gmail.com>
2020-01-29 17:36:28 -0300

    adicionado rotina de atualização de valores dos pedidos.

--- provider-project-backend/request/urls.py
+++ provider-project-backend/request/urls.py
@@ -1,13 +1,20 @@
 from rest_framework import routers
 from django.urls import path, include
 
-from .viewsets import ItensViewSet, RequestViewSet, StatusViewSet,requestsFromPdfViewSet
+from .viewsets import ItensViewSet, RequestViewSet, StatusViewSet,requestsFromPdfViewSet, routineRequestsSumItensViewSet
 
 router = routers.DefaultRouter()
 router.register(r'api/request', RequestViewSet , basename='RequestViewSet')
 router.register(r'api/request/itens', ItensViewSet , basename='ItensViewSet')
 router.register(r'api/request/status', StatusViewSet , basename='StatusViewSet')
 router.register(r'api/request/print/pdf', requestsFromPdfViewSet , basename='requestsFromPdfViewSet')
+
+
+#  rotinas para consertos via code.
+router.register(r'api/request/routine/settings-value-total-request', routineRequestsSumItensViewSet , basename='routineRequestsSumItensViewSet')
+
+
+
 
 urlpatterns = [
     path('', include(router.urls)),
