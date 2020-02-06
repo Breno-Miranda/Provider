e92f419d41691b4022c80abcca90386ecaf35689
Breno Miranda <brenomirandaster@gmail.com>
2020-02-05 22:28:46 -0300

    adicionando lote e campanha

--- provider-project-backend/request/serializers.py
+++ provider-project-backend/request/serializers.py
@@ -9,6 +9,7 @@ from users.serializers import UsersSerializer, ProfileSerializers
 from company.serializers import CatalogCompanySerializer
 from campaign.serializers import CampaignSerializers
 from users.serializers import UserBindProfileSerializers
+from lote.serializers import LotsSerializers
 
 class StatusSerializers(ModelSerializer):
     
@@ -30,14 +31,15 @@ class RequestSerializers(ModelSerializer):
     _status = StatusSerializers(source='status', read_only=True)
     _catalog = CatalogCompanySerializer(source='catalog', read_only=True)
     _campaign = CampaignSerializers(source='campaign', read_only=True)
-
+    _lot = LotsSerializers(source='lot', read_only=True)
+    
     create_date = serializers.DateTimeField(format="%d-%m-%Y")
 
     class Meta:
         model = Request
-        fields = ("id", "company", "lot",  "profile", "user_bind_typeit", "create_date",  "billing_date",  "payment_date",
+        fields = ("id", "company",  "profile", "user_bind_typeit", "create_date",  "billing_date",  "payment_date",
                   "return_date", "payment_methods", "amount", "amount_paid",  "amount_commission",  "commission_customer",
-                  "freight",  "discount",  "request_number", "observation", "_profile" , "_status", "_catalog" , "_campaign")
+                  "freight",  "discount",  "request_number", "observation", "_profile" , "_status", "_catalog" , "_campaign", "_lot")
 
     def get_create_date(self, obj):
         return str(obj.create_date.strftime("%d-%m-%Y"))
