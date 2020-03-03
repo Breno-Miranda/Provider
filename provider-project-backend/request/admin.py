0660fb841e75a91d1ce72fb678d5a2789f560644
Vitoriano <vitorianoernandes2012@gmail.com>
2020-03-03 12:10:26 -0300

    models users all fields add to admin.site

--- provider-project-backend/request/admin.py
+++ provider-project-backend/request/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
-
-# Register your models here.
 from .models import Request , Itens , Status
 
+
+admin.site.empty_value_display = '(empty)'
+
+
+class BindAdmin(admin.ModelAdmin):
+    list_display = ('user', 'company', 'type', 'team', 'sector', 'user_link', 'is_active')
 
 admin.site.register(Request)
 admin.site.register(Itens)
--- provider-project-backend/users/admin.py
+++ provider-project-backend/users/admin.py
@@ -1,12 +1,31 @@
 from django.contrib import admin
 from .models import Bind, Type, Team, Sector , Contact, Profile, Bank_Account
 
+admin.site.empty_value_display = '(empty)'
 
-admin.site.register(Bind)
-admin.site.register(Type)
-admin.site.register(Team)
-admin.site.register(Sector)
+
+class BindAdmin(admin.ModelAdmin):
+    list_display = ('user', 'company', 'type', 'team', 'sector', 'user_link', 'is_active')
+
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('bind', 'user', 'full_name', 'company', 'cpf')
+
+class TypeAdmin(admin.ModelAdmin):
+    list_display = ('type', 'code', 'is_active')
+
+class TeamAdmin(admin.ModelAdmin):
+    list_display = ('name', 'company', 'descripiton', 'number', 'is_active')
+
+class SectorAdmin(admin.ModelAdmin):
+    list_display = ('name', 'company', 'descripiton', 'number', 'is_active')
+
+
+
+admin.site.register(Bind, BindAdmin)
+admin.site.register(Type, TypeAdmin)
+admin.site.register(Team, TeamAdmin)
+admin.site.register(Sector, SectorAdmin)
 admin.site.register(Contact) 
 admin.site.register(Bank_Account) 
-admin.site.register(Profile)
+admin.site.register(Profile, ProfileAdmin)
 
